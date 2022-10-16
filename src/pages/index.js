1bf46ef9e8070dfa79e38f9c90b67672d64df126
aziv7 <aziz.jerbi@esprit.tn>
2022-10-16 12:26:11 +0200

    auto slider component for the banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Header } from '../components';
+import { Banner, Header } from '../components';
 
 export default function Home() {
   return (
@@ -9,6 +9,11 @@ export default function Home() {
       </Head>
 
       <Header />
+      <main className='max-w-screen-2xl mx-auto'>
+        {/**Banner */}
+        <Banner />
+        {/**Products */}
+      </main>
     </div>
   );
 }
